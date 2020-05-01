9c38ec4957de1db3c43b7d31fe96916f71685a01
Zafichi <zafichi@gmail.com>
2020-05-02 02:12:05 +0300

    heroku db

--- fresh_news.py
+++ fresh_news.py
@@ -7,10 +7,10 @@ import time
 bot_token = '1299904634:AAE9Ni1mQ2FPifpzulfcXETziWB4kEKhqjw'
 
 connection = psycopg2.connect(
-  database="telegram_fresh_news",
-  user="telefresh",
-  password="Freshman2104",
-  host="127.0.0.1",
+  database="d6a17hvofd05vd",
+  user="cfvdvpggurdcyc",
+  password="798725c679f1e5e7a362e6e57e037a7632ace5b6eac8d09167588bad5ab58131",
+  host="ec2-46-137-156-205.eu-west-1.compute.amazonaws.com",
   port="5432"
 )
 cur = connection.cursor()
